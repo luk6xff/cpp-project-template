c2b49921cdb7da52bb5fd8d3b6fd9ae5ebdfa5f5
luk6xff <lukasz.uszko@gmail.com>
2024-04-21 14:26:08 +0200

    That still works

--- app/build.sh
+++ app/build.sh
@@ -1,6 +1,10 @@
 rm -rf build
-TOOLCHAIN="Toolchain_x86_64.cmake"
-cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug  -DCMAKE_TOOLCHAIN_FILE="cmake/${TOOLCHAIN}" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-cmake --build build
-#cmake -DCMAKE_BUILD_TYPE=Debug  -DCMAKE_TOOLCHAIN_FILE="cmake/${TOOLCHAIN}" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DPROJECT_TARGET="my_project" .. && make
-#cmake --build build
+# TOOLCHAIN="Toolchain_x86_64.cmake"
+# cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug  -DCMAKE_TOOLCHAIN_FILE="cmake/${TOOLCHAIN}" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+# cmake --build build
+
+
+
+
+cmake -S . -B build
+cmake --build build --config Release && ./build/my_project
